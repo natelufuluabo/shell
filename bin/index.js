59cdb3a5694de42b607440f5fdb1e349bf3e0f3c
aircongo <natelufuluabo@yahoo.ca>
2023-12-15 04:59:36 -0500

    implemented step 1 of the challenge

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
-console.log('Shell starting')
+import readline from 'readline';
+import { exec } from 'child_process';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('ccsh> ', (command) => {
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing command: ${error.message}`);
+          return;
+        }
+      
+        if (stderr) {
+          console.error(`Command stderr: ${stderr}`);
+          return;
+        }
+      
+        console.log(`${stdout}`);
+    });
+    rl.close();
+});
